9212e7822d868a6404bdc3b6be8e977a156b6ca4
Larabsg18 <lara.beatriz.soares03@aluno.ifce.edu.br>
2020-08-21 00:26:02 +0000

    Script para atividade

--- hello.py
+++ hello.py
@@ -1,1 +1,24 @@
-print('hello')
+nome = 'Lara'
+idade = '18'
+curso = 'Técnico em Informática'
+semestre = 'S3'
+local = 'IFCE'
+
+print('Nome: ', nome,'\nIdade: ', idade,'\nCurso: ', curso,'\nSemestre: ', semestre,'\nLocal: ', local)
+
+materias = ['Prat.Prof', 'Eng.Sof', 'Empreen.', 'Redes', 'TAP', 'DAD']
+
+print('Disciplinas: ', materias)
+
+professores = {
+
+    'PP': 'Silas Santiago',
+    'E.S': 'Roberto Façanha',
+    'Empr.': 'Ricardo Cesar',
+    'Redes': 'Odara Feitosa',
+    'TAP': 'Felipe Bastos',
+    'DAD': 'Felipe Bastos'
+
+}
+
+print('Profs: ', professores)
